7ea2b39c5d31391d67fa413c5b70f8312d10d052
Haoxiang Lin <chrislhx@gmail.com>
2019-04-08 18:01:27 +0800

    Update check_sex_samtools.sh

--- check_sex_samtools.sh
+++ check_sex_samtools.sh
@@ -2,6 +2,6 @@
 xcov=$(echo "scale=4; $(samtools idxstats $1 | grep "X" | cut -f 3))
 ycov=$(echo "scale=4; $(samtools idxstats $1 | grep "Y" | cut -f 3))
 rat=$(echo "scale=4; ${xcov}/${ycov}" | bc)
-echo "$xcov $ycov $rat"
+echo "$1 $xcov $ycov $rat"
 
 #plot a graph and find the threshold
