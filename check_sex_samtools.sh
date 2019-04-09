97f27b5eb76596f71e6497cf0d2b4129b95cdaf2
Haoxiang Lin <chrislhx@gmail.com>
2019-04-09 10:58:36 +0800

    Update check_sex_samtools.sh

--- check_sex_samtools.sh
+++ check_sex_samtools.sh
@@ -1,6 +1,6 @@
 # only cov read on x an cov read y is needed to determinated to the sex
-xcov=$(echo "scale=4; $(samtools idxstats $1 | grep "X" | cut -f 3))
-ycov=$(echo "scale=4; $(samtools idxstats $1 | grep "Y" | cut -f 3))
+xcov=$(echo "scale=4; $(samtools idxstats $1 | grep "X" | cut -f 3)" | bc)
+ycov=$(echo "scale=4; $(samtools idxstats $1 | grep "Y" | cut -f 3)" | bc)
 rat=$(echo "scale=4; ${xcov}/${ycov}" | bc)
 echo "$1 $xcov $ycov $rat"
 
